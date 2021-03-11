a2eeee85b44517713898b14ca274437778fc6369
grierJonah <jjg8895@gmail.com>
2021-03-11 10:01:27 -0800

    Small edit

--- jonah-grier-mini-assignment5/src/index.js
+++ jonah-grier-mini-assignment5/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import Square from './square'
 import reportWebVitals from './reportWebVitals';
 import SquareGrid from './SquareGrid';
 
